c1b261a1316655ce845ab7f86ee086fb73bd1725
dongyeop00 <ehd_duq94@naver.com>
2024-01-19 06:53:58 +0900

    2024.01.19

---
+++ src/test/java/com/gdy/board_project/DygApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdy.board_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DygApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
