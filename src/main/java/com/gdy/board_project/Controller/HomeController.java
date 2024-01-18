22e5a2ed949ac32236aa83ba2ac62b89f5fb256c
dongyeop00 <ehd_duq94@naver.com>
2024-01-19 06:54:20 +0900

    feat : join, login

---
+++ src/main/java/com/gdy/board_project/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.gdy.board_project.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
